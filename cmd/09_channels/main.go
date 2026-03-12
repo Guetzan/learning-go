75e7166575cd72f1158e555243813fe9502964c4
Clovis <orcsdzn@gmail.com>
2026-03-12 13:05:41 -0300

    adiciona segunda parte das anotacoes da aula 9

--- cmd/09_channels/main.go
+++ cmd/09_channels/main.go
@@ -1,20 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var MAX_DDR5_RAM_PRICE float32 = 500
+var MAX_BOMB_APU_PRICE float32 = 80
 
 func main() {
-	var channel = make(chan int)
-	go update(channel)
+	startedAt := time.Now()
 
-	for i := range channel {
-		fmt.Println(i)
+	var ramChannel = make(chan string)
+	var apuChannel = make(chan string)
+	var websites = []string{"kabum.com.br", "amazon.com.br", "terabyteshop.com.br"}
+
+	for _, website := range websites {
+		go checkRamPrices(website, ramChannel)
+		go checkApuPrices(website, apuChannel)
+	}
+
+	notify(ramChannel, apuChannel)
+	fmt.Println(time.Since(startedAt))
+}
+
+func checkRamPrices(website string, ramChannel chan string) {
+	for {
+		time.Sleep(time.Second*1)
+		var price float32 = rand.Float32() * 10000
+
+		if price <= MAX_DDR5_RAM_PRICE {
+			ramChannel <- website
+			break
+		}
 	}
 }
 
-func update(channel chan int) {
-	defer close(channel)
+func checkApuPrices(website string, apuChannel chan string) {
+	for {
+		time.Sleep(time.Second*1)
+		var price float32 = rand.Float32()*1000
 
-	for i := range 5 {
-		channel <- i
+		if price <= MAX_BOMB_APU_PRICE {
+			apuChannel <- website
+			break
+		}
+	}
+}
+
+func notify(ramChannel chan string, apuChannel chan string) {
+	select {
+		case website := <- ramChannel:
+			fmt.Println("A RAM deal was found at", website)
+		case website := <- apuChannel:
+			fmt.Println("A APU deal was found at", website)
 	}
 }
